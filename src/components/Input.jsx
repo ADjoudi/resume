0077005730791f12a222a48280896de33865e576
Abdelouahab <ad.dev.main@gmail.com>
2023-08-08 14:22:26 +0100

    added costum input with styling

---
+++ src/components/Input.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import "./Input.css";
+
+function Input({ id = "input", name = "input", inputValue = "" }) {
+  const addLabelAnimation = (e) => {
+    if (!e.target.nextElementSibling.classList.contains("label-up")) {
+      e.target.nextElementSibling.classList.add("label-up");
+    }
+  };
+  const handleLabelAnimation = (e) => {
+    if (e.target.value.length <= 0) {
+      e.target.nextElementSibling.classList.remove("label-up");
+    }
+  };
+  return (
+    <div className="input-container">
+      <input
+        id={id}
+        name={inputValue}
+        onFocus={addLabelAnimation}
+        onBlur={handleLabelAnimation}
+      />
+      <label htmlFor={id}>{name}</label>
+    </div>
+  );
+}
+export default Input;
